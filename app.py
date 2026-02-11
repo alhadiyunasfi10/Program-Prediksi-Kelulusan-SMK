85ca301448b92519479bd531981f49f8b3418f2c
alhadiyunasfi10 <156499189+alhadiyunasfi10@users.noreply.github.com>
2026-02-11 20:06:00 +0700

    Update railway

--- app.py
+++ app.py
@@ -765,16 +765,7 @@ def logout():
     return redirect(url_for("login"))
 
 # ======================
-# RUN
+# RUN (LOCAL ONLY)
 # ======================
 if __name__ == "__main__":
-    app.run(debug=True)
-
-import os
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
-
-
-
-
+    app.run(host="0.0.0.0", port=5000, debug=True)
